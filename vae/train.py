b4b0db4c4a676dec39e1672eeb756e48ecfc329d
mokira3d48 <arnoldmokira3d48@gmail.com>
2025-05-21 15:49:12 -0800

    VAE impl: Encoder model is done

--- vae/train.py
+++ vae/train.py
@@ -12,6 +12,8 @@ import logging
 import torch
 import torch.nn.functional as F
 from torch import nn
+from torchinfo import summary
+
 from torch.utils import data
 
 # Set up logging
@@ -24,6 +26,7 @@ logging.basicConfig(
     ]
 )
 logger = logging.getLogger(__name__)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 class SelfAttention(nn.Module):
@@ -98,6 +101,8 @@ def test_self_attention():
     self_attn = SelfAttention(d_model=128, n_heads=8)
     inputs = torch.randn((16, 1000, 128))
     outputs = self_attn(inputs)
+
+    assert outputs.shape == (16, 1000, 128)
     logger.info(str(outputs.shape))
 
 
@@ -143,9 +148,151 @@ def test_attention_block():
     attn_block = AttentionBlock(num_channels=256)
     inputs = torch.randn((16, 256, 7, 7))
     outputs = attn_block(inputs)
+
+    assert outputs.shape == (16, 256, 7, 7)
     logger.info(str(outputs.shape))
 
 
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, num_groups=32):
+        super().__init__()
+        self.group_norm1 = nn.GroupNorm(num_groups, in_channels)
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+        self.group_norm2 = nn.GroupNorm(num_groups, out_channels)
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1,
+                padding=0,
+            )
+
+    def forward(self, x):
+        """
+        Forward pass
+        ------------
+
+        :param x: [batch_size, in_channels, h, w];
+        :returns: [batch_size, in_channels, h, w];
+
+        :type x: torch.Tensor
+        :rtype: torch.Tensor
+        """
+        residue = x.clone()
+
+        x = self.group_norm1(x)
+        x = F.relu(x)
+        x = self.conv1(x)
+        x = self.group_norm2(x)
+        x = self.conv2(x)
+
+        out = x + self.residual_layer(residue)
+        return out
+
+
+def test_residual_block():
+    res_block = ResidualBlock(in_channels=128, out_channels=256)
+    inputs = torch.randn((16, 128, 14, 14))
+    outputs = res_block(inputs)
+
+    assert outputs.shape == (16, 256, 14, 14)
+    logger.info(str(outputs.shape))
+
+
+class Encoder(nn.Sequential):
+    def __init__(self, num_channels=3, num_groups=32, scale_factor=0.18215):
+        # Conv2D(in_ch, out_ch, ks, s, p)
+        super().__init__(
+            nn.Conv2d(num_channels, 128, 3, 1, 1), # [n, 128, h, w]
+            ResidualBlock(128, 128, num_groups),   # [n, 128, h, w]
+            nn.Conv2d(128, 128, 3, 2, 0),          # [n, 128, h / 2, w / 2]
+
+            ResidualBlock(128, 256, num_groups), # [n, 256, h / 2, w / 2]
+            ResidualBlock(256, 256, num_groups), # [n, 256, h / 2, w / 2]
+            nn.Conv2d(256, 256, 3, 2, 0),        # [n, 256, h / 4, w / 4]
+
+            ResidualBlock(256, 512, num_groups), # [n, 512, h / 4, w / 4]
+            ResidualBlock(512, 512, num_groups), # [n, 512, h / 4, w / 4]
+            nn.Conv2d(512, 512, 3, 2, 0),        # [n, 256, h / 8, w / 8]
+
+            ResidualBlock(512, 512, num_groups),  # [n, 512, h / 8, w / 8]
+            ResidualBlock(512, 512, num_groups),  # [n, 512, h / 8, w / 8]
+            ResidualBlock(512, 512, num_groups),  # [n, 512, h / 8, w / 8]
+
+            AttentionBlock(512),                  # [n, 512, h / 8, w / 8]
+            ResidualBlock(512, 512, num_groups),  # [n, 512, h / 8, w / 8]
+            nn.GroupNorm(num_groups, 512),        # [n, 512, h / 8, w / 8]
+            nn.SiLU(),                            # [n, 512, h / 8, w / 8]
+
+            nn.Conv2d(512, 8, 3, 1, 1),  # [n, 8, h / 8, w / 8]
+            nn.Conv2d(8, 8, 1, 1, 0),    # [n, 8, h / 8, w / 8]
+
+        )
+
+        self.scale_factor = scale_factor
+
+    def forward(self, x):
+        """
+        Forward pass
+        ------------
+
+        :param x: [batch_size, num_channels, h, w],
+          num_channels can be equal to 3, if the images is in RGB,
+          or it can be equal to 1, if the images is in Gray scale.
+        :returns: tensor with [batch_size, 4, h / 8, w / 8] representing
+          the latent representation encoded.
+
+        :type x: torch.Tensor
+        :rtype: torch.Tensor
+        """
+        for module in self:
+            if isinstance(module, nn.Conv2d) and module.stride == (2, 2):
+                x = F.pad(x, (0, 1, 0, 1))  # (left, right, top, bottom)
+            x = module(x)
+
+        # We split the tensor x with dim [n, 8, h / 8, w / 8] into two tensors
+        #   of equal dimensions: [n, 4, h / 8, w / 8]
+        mean, log_variance = torch.chunk(x, 2, dim=1)
+
+        # Clamp log variance between -30 and 20
+        log_variance = torch.clamp(log_variance, -30, 20)
+
+        # Re-parameterization trick
+        std = torch.exp(0.5 * log_variance)
+        eps = torch.randn_like(std)
+        x = mean + eps * std
+
+        out = x * self.scale_factor
+        return out
+
+
+def test_encoder():
+    encoder = Encoder(num_channels=3)
+    encoder = encoder.to(device)
+
+    inputs = torch.randn((1, 3, 224, 224))
+    inputs = inputs.to(device)
+    summary(encoder, input_data=inputs)
+
+    inputs = torch.randn((4, 3, 224, 224))
+    outputs = encoder(inputs)
+    assert outputs.shape == (4, 4, 28, 28)
+    logger.info(str(outputs.shape))
 
 
 
